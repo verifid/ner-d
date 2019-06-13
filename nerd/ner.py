7d0a2780fbd559d9fdaf934116b98de48c74fef3
Abdullah Selek <abdullahselek@gmail.com>
2019-06-13 15:31:59 +0100

    Remove nltk usage.

--- nerd/ner.py
+++ nerd/ner.py
@@ -4,24 +4,16 @@
 import os
 import sys
 import subprocess
-import nltk
 import spacy
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
-
-nltk.download('all')
 
 __download_url__ = "https://github.com/explosion/spacy-models/releases/download"
+__version__ = '2.1.0'
 supported_languages = ['en_core_web_sm', 'de_core_news_sm', 'fr_core_news_sm',
                        'es_core_news_sm', 'pt_core_news_sm', 'it_core_news_sm',
                        'nl_core_news_sm', 'el_core_news_sm', 'xx_ent_wiki_sm']
 
-def download_model(filename, user_pip_args=None):
-    download_url = __download_url__ + "/" + filename
-    pip_args = ["--no-cache-dir", "--no-deps"]
-    if user_pip_args:
-        pip_args.extend(user_pip_args)
-    cmd = [sys.executable, "-m", "pip", "install"] + pip_args + [download_url]
+def download_model(filename):
+    cmd = [sys.executable, '-m', 'spacy', 'download', filename]
     return subprocess.call(cmd, env=os.environ.copy())
 
 def load_model(language='en_core_web_sm'):
@@ -46,26 +38,3 @@ def load_model(language='en_core_web_sm'):
     download_model(language)
     nlp = spacy.load(language)
     return nlp
-
-def preprocess(text, lang_tokenize='english', lang_tag='eng'):
-    """
-    Preprocesses given text with optional language parameter.
-
-        >>> from nerd import ner
-        >>> ner.preprocess('The four-time Tour de France champion, 34,
-        has suffered a fractured right femur, a broken hip, a fractured
-        elbow and fractured ribs and lost consciousness following the crash.')
-
-    :param text: Sequence text.
-    :type text: str
-    :param lang_tokenize: the model name in the Punkt corpus
-    :type lang_tokenize: str
-    :param lang: the ISO 639 code of the language, e.g. 'eng' for English, 'rus' for Russian
-    :type lang: str
-    :return: The tagged tokens
-    :rtype: list(tuple(str, str))
-    """
-
-    text = nltk.word_tokenize(text)
-    text = nltk.pos_tag(text)
-    return text
--- tests/test_ner.py
+++ tests/test_ner.py
@@ -9,14 +9,6 @@ from nerd import ner
 
 class NerTest(unittest.TestCase):
 
-    def test_preprocess(self):
-        result = ner.preprocess('The four-time Tour de France champion, 34,'
-        + 'has suffered a fractured right femur, a broken hip, a fractured'
-        + 'elbow and fractured ribs and lost consciousness following the crash.')
-        self.assertEqual(result, [('The', 'DT'), ('four-time', 'JJ'), ('Tour', 'NNP'), ('de', 'FW'), ('France', 'NNP'), ('champion', 'NN'), (',', ','), ('34', 'CD'), (',', ','), ('has', 'VBZ'), ('suffered', 'VBN'), ('a', 'DT'), ('fractured', 'JJ'), ('right', 'NN'), ('femur', 'NN'), (',', ','), ('a', 'DT'), ('broken', 'JJ'), ('hip', 'NN'), (',', ','), ('a', 'DT'), ('fracturedelbow', 'NN'), ('and', 'CC'), ('fractured', 'VBD'), ('ribs', 'NNS'), ('and', 'CC'), ('lost', 'VBN'), ('consciousness', 'NN'), ('following', 'VBG'), ('the', 'DT'), ('crash', 'NN'), ('.', '.')])
-        result = ner.preprocess('Michael Jordan born 17.02.1963 in New York is an American basketballer.')
-        self.assertEqual(result, [('Michael', 'NNP'), ('Jordan', 'NNP'), ('born', 'VBD'), ('17.02.1963', 'CD'), ('in', 'IN'), ('New', 'NNP'), ('York', 'NNP'), ('is', 'VBZ'), ('an', 'DT'), ('American', 'JJ'), ('basketballer', 'NN'), ('.', '.')])
-
     def test_load_model(self):
         nlp = ner.load_model()
         self.assertIsNotNone(nlp)

0c15d27d4d3dc9fb2fbb3c2a853f0093a7604ac8
Abdullah Selek <abdullahselek@users.noreply.github.com>
2019-06-19 14:42:12 +0300

    Change print outputs.

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -15,4 +15,4 @@ class NerTest(unittest.TestCase):
 
     def test_name(self):
         doc = ner.name('GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features.')
-        print(doc)
+        print([(X.text, X.label_) for X in doc])

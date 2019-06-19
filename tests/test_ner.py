d692f1e70be51497ddd1ceb8e8868e36f6d9bc9f
Abdullah Selek <abdullahselek@users.noreply.github.com>
2019-06-19 14:12:45 +0300

    Compare doc.

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -15,4 +15,5 @@ class NerTest(unittest.TestCase):
 
     def test_name(self):
         doc = ner.name('GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features.')
+        self.assertEqual(doc, 'GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features.')
         print(doc.ents)

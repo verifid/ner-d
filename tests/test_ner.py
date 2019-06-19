f658a62936d2d6add8b107d619bbe7c7cfdbfa93
Abdullah Selek <abdullahselek@users.noreply.github.com>
2019-06-19 15:01:23 +0300

    Compare text and labels.

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -15,4 +15,5 @@ class NerTest(unittest.TestCase):
 
     def test_name(self):
         doc = ner.name('GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features.')
-        print([(X.text, X.label_) for X in doc])
+        text_label = [(X.text, X.label_) for X in doc]
+        self.assertEqual(text_label, [(u'GitHub', u'ORG'), (u'April 10, 2008', u'DATE'), (u'Microsoft', u'ORG'), (u'American', u'NORP'), (u'Git', u'PERSON'), (u'SCM', u'ORG'), (u'Git', u'PERSON')])

e91aa32907a02284ec6ff89842256a7a75e770bc
Abdullah Selek <abdullahselek@gmail.com>
2019-06-24 09:57:29 +0100

    Use multiline string.

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -14,6 +14,8 @@ class NerTest(unittest.TestCase):
         self.assertIsNotNone(nlp)
 
     def test_name(self):
-        doc = ner.name('GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality of Git as well as adding its own features.')
+        doc = ner.name("""GitHub launched April 10, 2008, a subsidiary of Microsoft, is an American web-based hosting service for version control using Git.
+                       It is mostly used for computer code. It offers all of the distributed version control and source code management (SCM) functionality
+                       of Git as well as adding its own features.""", language='en_core_web_sm')
         text_label = [(X.text, X.label_) for X in doc]
         self.assertEqual(text_label, [(u'GitHub', u'ORG'), (u'April 10, 2008', u'DATE'), (u'Microsoft', u'ORG'), (u'American', u'NORP'), (u'Git', u'PERSON'), (u'SCM', u'ORG'), (u'Git', u'PERSON')])

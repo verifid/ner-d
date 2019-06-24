51c68732e5c23909725c5c404a7a7e0c3c579ee3
Abdullah Selek <abdullahselek@gmail.com>
2019-06-24 08:41:13 +0100

    Update docs.

--- nerd/ner.py
+++ nerd/ner.py
@@ -15,6 +15,19 @@ supported_languages = ['en_core_web_sm', 'de_core_news_sm', 'fr_core_news_sm',
                        'nl_core_news_sm', 'el_core_news_sm', 'xx_ent_wiki_sm']
 
 def download_model(model_name):
+    """
+    Downloads and links language trained model.
+
+        >>> from nerd import ner
+        >>> ner.download_model(model_name='en_core_web_sm')
+        >>> supported languages 'en_core_web_sm', 'de_core_news_sm', 'fr_core_news_sm',
+        >>> 'es_core_news_sm', 'pt_core_news_sm', 'it_core_news_sm',
+        >>> 'nl_core_news_sm', 'el_core_news_sm', 'xx_ent_wiki_sm'
+
+    :param model_name: Model package name.
+    :type model_name: str
+    """
+
     download(model_name)
     package_path = get_package_path(model_name)
     link(model_name, model_name, force=True, model_path=package_path)
@@ -28,8 +41,8 @@ def load_model(language='en_core_web_sm'):
         >>> or
         >>> ner.load_model(language='de_core_news_sm')
         >>> supported languages 'en_core_web_sm', 'de_core_news_sm', 'fr_core_news_sm',
-        'es_core_news_sm', 'pt_core_news_sm', 'it_core_news_sm',
-        'nl_core_news_sm', 'el_core_news_sm', 'xx_ent_wiki_sm'
+        >>> 'es_core_news_sm', 'pt_core_news_sm', 'it_core_news_sm',
+        >>> 'nl_core_news_sm', 'el_core_news_sm', 'xx_ent_wiki_sm'
 
     :param language: Language package name, shortcut link or model path.
     :type language: str
@@ -53,6 +66,7 @@ def name(text, language='en_core_web_sm'):
     :type text: str
     :param language: Language package name, shortcut link or model path.
     :type language: str
+    :rtype: Entities found from given text.
     """
 
     try:

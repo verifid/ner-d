a185244cf3e0696d487c9e153430506b80bb1df3
Abdullah Selek <abdullahselek@gmail.com>
2019-07-02 22:32:38 +0100

    Add an extra test.

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -17,3 +17,8 @@ class NerTest(unittest.TestCase):
                        of Git as well as adding its own features.""", language='en_core_web_sm')
         text_label = [(X.text, X.label_) for X in doc]
         self.assertEqual(text_label, [(u'GitHub', u'ORG'), (u'April 10, 2008', u'DATE'), (u'Microsoft', u'ORG'), (u'American', u'NORP'), (u'Git', u'PERSON'), (u'SCM', u'ORG'), (u'Git', u'PERSON')])
+        doc = ner.name("""Michael Jeffrey Jordan born February 17, 1963 in Brooklyn, New York, United States of America. Known by his initials, MJ,[5] is an American former professional
+                       basketball player who is the principal owner and chairman of the Charlotte Hornets of the National Basketball Association
+                       """, language='en_core_web_sm')
+        text_label = [(X.text, X.label_) for X in doc]
+        self.assertEqual(text_label, [('Michael Jeffrey Jordan', 'PERSON'), ('February 17, 1963', 'DATE'), ('Brooklyn', 'GPE'), ('New York', 'GPE'), ('United States of America', 'GPE'), ('MJ,[5', 'ORG'), ('American', 'NORP'), ('the Charlotte Hornets', 'LOC'), ('the National Basketball Association', 'ORG')])

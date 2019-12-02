e575c8909a68f9b7b1a8d06f26d66d2ce2c92cf3
Abdullah Selek <abdullahselek@gmail.com>
2019-12-02 19:22:35 +0000

    Bump version.

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2019, VerifID'
 author = u'Abdullah Selek'
 
 # The short X.Y version
-version = u'0.2'
+version = u'0.2.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.2'
+release = u'0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
